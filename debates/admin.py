8b335deba425a3aea5ff0311390c51717f95db73
Shlok Mishra <106074331+Shlok-B-Mishra@users.noreply.github.com>
2025-07-11 00:26:53 +0530

    feat: Register CustomUser and Debate models in Django admin

--- debates/admin.py
+++ debates/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+# debates/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Debate
+
+class DebateAdmin(admin.ModelAdmin):
+    # Customize how debates are displayed in the admin list
+    list_display = ('title', 'creator', 'status', 'scheduled_start_time')
+    # Add filters for easy sorting
+    list_filter = ('status', 'creator')
+    # Add a search bar
+    search_fields = ('title', 'description')
+
+admin.site.register(Debate, DebateAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+# users/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    # Add any fields you want to see in the admin list view
+    list_display = ['username', 'email', 'is_staff', 'is_active']
+    # Add custom fields to the admin form if you add any to the model later
+    # fieldsets = UserAdmin.fieldsets + (
+    #     (None, {'fields': ('your_custom_field',)}),
+    # )
+    # add_fieldsets = UserAdmin.add_fieldsets + (
+    #     (None, {'fields': ('your_custom_field',)}),
+    # )
+
+admin.site.register(CustomUser, CustomUserAdmin)
